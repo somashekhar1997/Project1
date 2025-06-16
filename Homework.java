4f492761d97e5b36cfaa79461fe6bcc7a0cda98c
somashekhar1997 <somashekharhatti123@gmail.com>
2025-06-16 13:20:52 +0530

    Add files via upload

---
+++ Homework.java
@@ -0,0 +1,40 @@
+package login;
+
+public class Homework 
+{
+	public static void main(String[] args) {
+		int maths=15;
+		int science=25;
+		int english=28;
+		int avg=(maths+science+english)/3;
+	
+
+  if(avg>=90) 
+  {
+	  System.out.println("Grade A+");
+	  System.out.println("Excellent");
+  }
+  else if(avg>=75)
+  {
+	  System.out.println("Grade A");
+	  System.out.println("Good");
+  }
+  else if(avg>=60)
+  {
+	  System.out.println("Grade B");
+  System.out.println("improve");
+  }
+  else if(avg>=40)
+  {
+	  System.out.println("Grade C");
+  System.out.println("improve");
+  }
+  else if(avg<35)
+  {
+	  if(maths<35 || science<35 || english<35)
+	  {
+		  System.out.println("fail");
+	  
+	  }}
+	}}
+	
