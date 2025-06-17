1779db3f0de38419f19b6176e766c6b02bd840c0
somashekhar1997 <somashekharhatti123@gmail.com>
2025-06-17 23:17:03 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,18 @@
+package login;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int age=25;
+   double percentage=76.12d;
+   char grade='A';
+   boolean isThisTrue=false;
+   
+   System.out.println("age is " +age);
+   System.out.println("percentage is " +percentage);
+   System.out.println("grade is " +grade);
+   System.out.println("isThisTrue is " +isThisTrue);
+	}
+
+}
