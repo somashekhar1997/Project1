274a5433d948fd6d2ebdbac18fc58dcabc3b9fbe
somashekhar1997 <somashekharhatti123@gmail.com>
2025-06-17 23:16:40 +0530

    Add files via upload

---
+++ Localoperator.java
@@ -0,0 +1,17 @@
+package login;
+
+public class Localoperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a=10;
+       int b=20;
+       int c=30;
+       int d=15;
+       
+       boolean value = (b>a) || (c>d);
+       System.out.println(value);
+       
+	}
+
+}
